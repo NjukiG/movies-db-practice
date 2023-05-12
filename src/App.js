3f4c23ca4b9464ef9c8527cdce1998111ada11b5
George <gnjuki19@gmail.com>
2023-05-12 16:04:51 +0300

    Able to render dynamically

--- src/App.js
+++ src/App.js
@@ -5,66 +5,13 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Atlanta",
-      Year: "2016–2022",
-      imdbID: "tt4288182",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZGU1MzRhNmMtNDExOS00NTk2LWJlYzMtMzc4YWYyN2Q3M2ZmXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg",
-    },
-    {
-      Title: "The Real Housewives of Atlanta",
-      Year: "2008–",
-      imdbID: "tt1252370",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMmVmM2VmMTctOGY5NS00NGU3LTlmNTItY2JkZWY3N2I2NWUyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    },
-    {
-      Title: "Love & Hip Hop: Atlanta",
-      Year: "2012–",
-      imdbID: "tt2224452",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMmZiMTI3N2ItZmIyMy00MDUzLTkyNDgtYzEyYWQwNTljNmY2XkEyXkFqcGdeQXVyMTMxNjUyMDkx._V1_SX300.jpg",
-    },
-    {
-      Title: "Say Yes to the Dress: Atlanta",
-      Year: "2010–",
-      imdbID: "tt1730755",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZTU1ZmM5NjgtYTM0My00Zjc3LWEyNDctNDI1MWM2YThiYTIyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Atlanta Child Murders",
-      Year: "1985",
-      imdbID: "tt0088750",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTQ1MzNmYjctNGJmOC00ODNhLTg1MDEtOTJlZDI3NmVkYzcyXkEyXkFqcGdeQXVyMTk0MjQ3Nzk@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
-  const URL = "http://www.omdbapi.com/?s=Atlanta&apikey=cc8b48eb";
+  const URL = "http://www.omdbapi.com/?s=Avengers&apikey=cc8b48eb";
   useEffect(() => {
     fetch(URL)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`Request failed with status code ${res.status}`);
-        } else {
-          res.json();
-        }
-      })
-      .then((data) => {
-        if (data) {
-          console.log(data);
-        } else {
-          console.log("No results found.");
-        }
-      });
+      .then((res) => res.json())
+      .then((data) => console.log(data.Search));
   }, []);
   return (
     <div className="container-fluid movie-container">
