b67ad6be307cca743f98d252e5379411f3d595df
George <gnjuki19@gmail.com>
2023-05-12 17:07:55 +0300

    Able to render the search bar, search movies and render some movies on load. i.e movies with the name Steel

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
-import "bootswatch/dist/slate/bootstrap.min.css"; // Added this :boom:
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import "bootswatch/dist/sketchy/bootstrap.min.css"; // Added this :boom:
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchName, setSearchName] = useState("");
 
-  const URL = "http://www.omdbapi.com/?s=Avengers&apikey=cc8b48eb";
-  useEffect(() => {
+  const getMovies = (searchName) => {
+    const URL = `http://www.omdbapi.com/?s=${!searchName ? "Steel" : searchName}&apikey=cc8b48eb`;
+
     fetch(URL)
       .then((res) => res.json())
-      .then((data) => console.log(data.Search));
-  }, []);
+      .then((data) => {
+        if (data.Search) {
+          setMovies(data.Search);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getMovies(searchName);
+  }, [searchName]);
   return (
     <div className="container-fluid movie-container">
       <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="My Movies" />
+        <SearchBox searchName={searchName} setSearchName={setSearchName} />
+      </div>
+      <div className="row">
         <MovieList movies={movies} />
       </div>
     </div>
---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SearchBox({ searchName, setSearchName }) {
+  return (
+    <div className="col col-sm-6">
+      <input
+        className="form-control"
+        placeholder="Search your movie here..."
+        value={searchName}
+        onChange={(e) => setSearchName(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
